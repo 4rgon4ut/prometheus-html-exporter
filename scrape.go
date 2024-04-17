d749b9c555665cad5bf833d373f5dff3af54649c
4rgon4ut <egorlaptev98@gmail.com>
2024-04-17 23:21:42 +0100

    feat: gnosisscan scrape and headers

--- scrape.go
+++ scrape.go
@@ -36,32 +36,46 @@ func scrape(config types.ScrapeConfig) (float64, error) {
 }
 
 func doRequest(url string) (io.ReadCloser, error) {
-	// @TODO: Allow passing headers, timeout and other request args
-	client := &http.Client{
-		// Timeout: 10000,
-	}
+    client := &http.Client{
+        // Timeout: 10 * time.Second, // Uncomment and adjust as necessary
+    }
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create request. error: %s", err)
-	}
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return nil, fmt.Errorf("unable to create request: %s", err)
+    }
 
-	req.Header.Add("User-Agent", fmt.Sprintf("prometheus-html-exporter/%s", BuildVersion))
+    // Add headers to mimic a browser request
+    req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+    req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
+    req.Header.Add("Accept-Encoding", "gzip, deflate, br") // Remove 'zstd' if it causes issues
+    req.Header.Add("Accept-Language", "en-GB,en;q=0.9")
+    req.Header.Add("Cache-Control", "max-age=0")
+    req.Header.Add("Sec-Fetch-Dest", "document")
+    req.Header.Add("Sec-Fetch-Mode", "navigate")
+    req.Header.Add("Sec-Fetch-Site", "cross-site")
+    req.Header.Add("Sec-Fetch-User", "?1")
+    req.Header.Add("Sec-Gpc", "1")
+    req.Header.Add("Upgrade-Insecure-Requests", "1")
+    req.Header.Add("Referer", "https://www.google.com/")
 
-	log.Infof("scraping page %s", url)
+	req.Header.Add("Cookie", "ASP.NET_SessionId=0unkgypizygltszqicuwwpln")
 
-	resp, err := client.Do(req)
+    log.Infof("Scraping page %s", url)
 
-	if err != nil {
-		return nil, fmt.Errorf("unable to request URL %s. error: %s", url, err)
-	}
+    resp, err := client.Do(req)
+    if err != nil {
+        return nil, fmt.Errorf("unable to request URL %s: %s", url, err)
+    }
+    defer resp.Body.Close()
 
-	if resp.StatusCode < 200 || resp.StatusCode > 400 {
-		return nil, fmt.Errorf("request error: %s", resp.Status)
-	}
+    if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+        return nil, fmt.Errorf("request error: %s", resp.Status)
+    }
 
-	return resp.Body, nil
+    return resp.Body, nil
 }
+
 
 func parseSelector(body io.ReadCloser, selector string) (string, error) {
 	doc, err := htmlquery.Parse(body)
